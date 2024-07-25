9218428bcf9a7eac6f05529323d84173c7340af0
thexeromin <thexeromin@gmail.com>
2024-07-25 20:13:08 +0530

    add: basic ui; format code

--- main.c
+++ main.c
@@ -17,8 +17,14 @@ char *choices[] = {
     "Choice 8",
     "Choice 9",
     "Choice 10",
-    "Exit",
-    (char *)NULL,
+    "Choice 11",
+    "Choice 12",
+    "Choice 13",
+    "Choice 14",
+    "Choice 15",
+    "Choice 16",
+    "Choice 17",
+    "Choice 18",
 };
 
 void print_in_middle(
@@ -36,6 +42,7 @@ int main() {
 	MENU* my_menu;
     WINDOW* my_menu_win;
     int n_choices, i;
+    int width, height, starty, startx;
 
     /* Initialize curses */
     initscr();
@@ -46,43 +53,43 @@ int main() {
 	init_pair(1, COLOR_RED, COLOR_BLACK);
 	init_pair(2, COLOR_CYAN, COLOR_BLACK);
 
+    width = COLS - 5;
+    height = LINES / 2;
+    startx = (COLS - width) / 2;
+    starty = 1;
+
 	/* Create items */
     n_choices = ARRAY_SIZE(choices);
     my_items = (ITEM**) calloc(n_choices, sizeof(ITEM*));
     for(i = 0; i < n_choices; ++i) {
-        my_items[i] = new_item(choices[i], choices[i]);
+        my_items[i] = new_item(" ", choices[i]);
     }
 
 	/* Crate menu */
 	my_menu = new_menu((ITEM**) my_items);
 
 	/* Create the window to be associated with the menu */
-    my_menu_win = newwin(10, 40, 4, 4);
+    my_menu_win = newwin(height, width, starty, startx);
     keypad(my_menu_win, TRUE);
      
 	/* Set main window and sub window */
     set_menu_win(my_menu, my_menu_win);
-    set_menu_sub(my_menu, derwin(my_menu_win, 6, 38, 3, 1));
-	set_menu_format(my_menu, 5, 1);
+    set_menu_sub(my_menu, derwin(my_menu_win, height - 4, width - 2, 3, 1));
+	set_menu_format(my_menu, height - 4, 1);
 			
-	/* Set menu mark to the string " * " */
-    set_menu_mark(my_menu, " * ");
+	/* Set menu mark to the string " *" */
+    set_menu_mark(my_menu, " *");
 
 	/* Print a border around the main window and print a title */
     box(my_menu_win, 0, 0);
-	print_in_middle(my_menu_win, 1, 0, 40, "My Menu", COLOR_PAIR(1));
+	print_in_middle(my_menu_win, 1, 0, COLS - 5, "Termzic", COLOR_PAIR(1));
 	mvwaddch(my_menu_win, 2, 0, ACS_LTEE);
-	mvwhline(my_menu_win, 2, 1, ACS_HLINE, 38);
-	mvwaddch(my_menu_win, 2, 39, ACS_RTEE);
+	mvwhline(my_menu_win, 2, 1, ACS_HLINE, width - 2);
+	mvwaddch(my_menu_win, 2, width - 1, ACS_RTEE);
         
 	/* Post the menu */
 	post_menu(my_menu);
 	wrefresh(my_menu_win);
-	
-	attron(COLOR_PAIR(2));
-	mvprintw(LINES - 2, 0, "Use PageUp and PageDown to scoll down or up a page of items");
-	mvprintw(LINES - 1, 0, "Arrow Keys to navigate (F1 to Exit)");
-	attroff(COLOR_PAIR(2));
 	refresh();
 
 	while((c = wgetch(my_menu_win)) != KEY_F(1)) {
